de0b5082d36165b66c8d0ddfd10a4d6cf998257f
angslhn <angslhn@gmail.com>
2024-10-07 11:13:10 +0700

    Belajar java dasar cara membuat switch statement.

---
+++ Pertemuan 6/SwitchStatement.java
@@ -0,0 +1,56 @@
+public class SwitchStatement {
+    public static void main(String[] args) {
+//      Switch Normal
+//        String nilai = "C";
+//
+//        switch (nilai) {
+//            case "A":
+//                System.out.println("Nilai Sangat Baik.");
+//                break;
+//            case "B":
+//            case "C":
+//                System.out.println("Nilai Cukup Baik.");
+//                break;
+//            case "D":
+//                System.out.println("Nilai Kurang Baik.");
+//                break;
+//            case "E":
+//                System.out.println("Nilai Tidak Baik.");
+//                break;
+//            default:
+//                System.out.println("Anda harus mengulang.");
+//                break;
+//        }
+
+//      Switch Lambda
+//        String nilai = "E";
+//
+//        switch (nilai) {
+//            case "A" -> System.out.println("Nilai Sangat Baik.");
+//            case "B", "C" -> System.out.println("Nilai Cukup Baik.");
+//            case "D" -> System.out.println("Nilai Kurang Baik.");
+//            case "E" -> System.out.println("Nilai Tidak Baik.");
+//            default -> {
+//                System.out.println("Anda harus mengulang.");
+//            }
+//
+//
+//      Switch Lambda Yield
+//        String nilai = "A";
+//        String ucapan = switch (nilai) {
+//            case "A":
+//                yield "Nilai Sangat Baik.";
+//            case "B", "C":
+//                yield  "Nilai Cukup Baik.";
+//            case "D":
+//                yield "Nilai Kurang Baik.";
+//            case "E":
+//                yield "Nilai Tidak Baik.";
+//            default :
+//                yield "Anda harus mengulang.";
+//        };
+
+        System.out.println(ucapan);
+
+    }
+}
