42470e420e79864c63060476c40820f908410172
angslhn <angslhn@gmail.com>
2024-10-08 18:13:59 +0700

    Belajar java dasar cara menggunakan for looping.

---
+++ Pertemuan 7/ForLooping.java
@@ -0,0 +1,9 @@
+public class ForLooping {
+    public static void main(String[] args) {
+//      For Looping
+        String no = "No. ";
+        for (var i = 1; i <= 10; i++) {
+            System.out.println(no + i);
+        }
+    }
+}
