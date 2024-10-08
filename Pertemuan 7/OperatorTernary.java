af1e70d9c3e498e23179d38f0525620c364f0f40
angslhn <angslhn@gmail.com>
2024-10-08 18:07:36 +0700

    Belajar java dasar cara menggunakan operator ternary.

---
+++ Pertemuan 7/OperatorTernary.java
@@ -0,0 +1,8 @@
+public class OperatorTernary {
+    public static void main(String[] args) {
+//      Operator Ternary
+        int jam = 11;
+        String ucapan = jam > 6 && jam <= 10 ? "Selamat Pagi!" : "Selamat Siang!";
+        System.out.println(ucapan);
+    }
+}
