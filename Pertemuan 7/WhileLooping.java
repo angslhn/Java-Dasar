e3efc186253f8785b1231ed5ba0574e06f472a06
angslhn <angslhn@gmail.com>
2024-10-08 18:43:23 +0700

    Belajar java dasar cara menggunakan while looping.

---
+++ Pertemuan 7/WhileLooping.java
@@ -0,0 +1,9 @@
+public class WhileLooping {
+    public static void main(String[] args) {
+        var i = 1;
+        while (i <= 10) {
+            System.out.println("No. " + i);
+            i++;
+        }
+    }
+}
