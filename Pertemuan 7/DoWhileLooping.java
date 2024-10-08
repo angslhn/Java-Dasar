f807de4c1629ba003103113bb6bac199c5d9d522
angslhn <angslhn@gmail.com>
2024-10-08 18:52:20 +0700

    Belajar java dasar cara menggunakan do while looping.

---
+++ Pertemuan 7/DoWhileLooping.java
@@ -0,0 +1,9 @@
+public class DoWhileLooping {
+    public static void main(String[] args) {
+        var i = 1;
+        do {
+            System.out.println("No. " + i);
+            i++;
+        } while (i <= 10);
+    }
+}
