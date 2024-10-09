5b5e912bbfc9acd7b9f136fe4d8251e9c39d108e
angslhn <angslhn@gmail.com>
2024-10-10 05:43:43 +0700

    Belajar java dasar cara menggunakan foreach.

---
+++ Pertemuan 8/Foreach.java
@@ -0,0 +1,10 @@
+public class Foreach {
+    public static void main(String[] args) {
+//      Foreach array
+        String[] pengguna = {"Aang Solihin", "Ujang Supardi", "Maman Supratman", "Cecep Hidayat"};
+
+        for (var paraPengguna : pengguna) {
+            System.out.println(paraPengguna);
+        }
+    }
+}
