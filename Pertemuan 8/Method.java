bee054ad3667aa86fbeadc81aa855b8ee76c1b51
angslhn <angslhn@gmail.com>
2024-10-10 07:46:11 +0700

    Belajar java dasar cara membuat sebuah method atau function.

---
+++ Pertemuan 8/Method.java
@@ -0,0 +1,9 @@
+public class Method {
+    public static void main(String[] args) {
+        showHelloWorld();
+    }
+
+    static void showHelloWorld() {
+        System.out.println("Hello World!");
+    }
+}
