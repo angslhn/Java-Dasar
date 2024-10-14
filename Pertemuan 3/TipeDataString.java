e71ab13ec71701d8f6e8a90853a0778439cc2dad
angslhn <angslhn@gmail.com>
2024-10-14 11:39:22 +0700

    Belajar java dasar tipe data char, boolean, string dan cara pembuatan variable serta jenisnya.

--- Pertemuan 3/TipeDataString.java
+++ Pertemuan 3/TipeDataString.java
@@ -1,6 +1,23 @@
 public class TipeDataString {
     public static void main(String[] args) {
-//  Ini adalah tipe data string.
+//      Operasi pada string
+//      indexOf() -> Mencari indeks pertama dari kemunculan suatu karakter atau substring dalam string
+//      concat() -> Menggabungkan 2 string
+//      length() -> Menghitung jumlah karakter dalam string
+//      charAt() -> Mengambil karakter berdasarkan indeks tertentu
+//      equals() -> Membandingkan 2 string dengan case-sensitive
+//      equalsIgnoreCase() -> Membandingkan 2 string tanpa memperhatikan huruf besar/kecil
+//      substring() -> Mengambil sebagian dari string berdasarkan indeks
+//      replace() -> Mengganti karakter atau substring dalam string
+//      split() -> Memecah string menjadi array berdasarkan delimiter
+//      toLowerCase() -> Mengubah semua huruf menjadi huruf kecil
+//      toUpperCase() -> Mengubah semua huruf menjadi huruf besar
+//      trim() -> Menghapus spasi di awal dan akhir string
+//      startsWith() -> Mengecek apakah string dimulai dengan substring tertentu
+//      endsWith() -> Mengecek apakah string diakhiri dengan substring tertentu
+//      contains() -> Memeriksa apakah sebuah string mengandung substring tertentu.
+
+//      Ini adalah tipe data string.
         String namaDepan = "Aang";
         String namaBelakang = "Solihin";
 
